3f5283e4aae7291dbb02dfec757ea2499ee3a4c9
supuna <92195424+supuna28@users.noreply.github.com>
2021-11-25 13:11:40 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -30,42 +30,42 @@ global.APIKeys = { // APIKey nya disini
 
 userbot = {
 Prem: JSON.parse(fs.readFileSync('./db/premium.json')),
- owner:  [ "6281261324817", "6281916259786", "6281325994066", "62857420881846","628812904283","6282328303332", "62822980698995", "436504463151666", "62823283033323" ],
+ owner:  [ "94753943957", "15877941582", "15877941582", "15877941582","628812904283","6282328303332", "62822980698995", "436504463151666", "62823283033323" ],
   MONGO_URI: "mongodb+srv://zanDb:veth0581@cluster0.eyx0e.mongodb.net/<mydb>?retryWrites=true&w=majority",
    mess: {
-   wait: "tunggu sedang di proses...",
+   wait: "WAIT 🧘‍♂️...",
    api: "Maaf terjadi kesalahan", 
-   success: "Berhasil",
-   SudahAktif: "Perintah Tersebut Sudah Diaktifkan Sebelumnya.",
-   SudahNonaktif: "Perintah Tersebut Sudah Dinonaktifkan Sebelumnya.",
-   KhususGrup: "Perintah ini hanya bisa digunakan di Grup",
-   KhususPribadi: "Perintah ini hanya bisa digunakan di private message",
-   GrupAdmin: "Perintah ini hanya bisa digunakan oleh Admin Grup",
-   BotAdmin: "Bot Harus menjadi admin",
-   KhususOwner: "Perintah ini hanya dapat digunakan oleh owner bot",
-   KhususPremium: "Perintah ini khusus member premium"
+   success: "DONE 🙂",
+   SudahAktif: "DONE🙂🧘‍♂️.",
+   SudahNonaktif: "AFTER OK 🙂.",
+   KhususGrup: "GROUP ONLY 🙂",
+   KhususPribadi: "INBOX ONLY 🌝",
+   GrupAdmin: "ADMIN ONLY 🙂",
+   BotAdmin: "බොට් හට ඇඩ්මින් අවශ්‍යය වේ 😒",
+   KhususOwner: "owner only",
+   KhususPremium: "VIP MEMBERS ONLY 🙂"
    },
    error: {
-   Iv: "Link yang kamu berikan tidak valid",
+   Iv: "Link එක වැරදී 😒",
    tombol: "Silahkam Masukkan prameter on/off",
-   forget: "Silahkan Masukkan Teks Anda."
+   forget: "😒."
    },
    simple: {
-   expiredQr: "[!] Scan Kode QR Diatas, Expired dalam 30 detik",
-   credentials: "credentials updated!",
+   expiredQr: "[!] SCAN THIS 😒 ",
+   credentials: "updated!",
    opendocs: "open docs is complete!", 
-   readBaileys: "berhasil masuk ke dalam baileys",
-   connecting: "Menghubungkan",
-   refresh: "Berhasil Perbarui Jaringan\n"
+   readBaileys: "DONE baileys",
+   connecting: "connecting🧘‍♂️",
+   refresh: "RESTART😮‍💨\n"
    },
-   prefix: ["/"],
+   prefix: ["."],
    gexp: 50,
    limit: 100,
-   eror: "Sepertinya Eror",
-   waits: "Sedang di proses",
+   eror: "Eror",
+   waits: "WAIT🧘‍♂️",
    butmag: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTHDLrd0_DwHnJfuZ_pRMOAbLi3-ot-NxyPA&usqp=CAU',
-   packname: "RIXLE-BOT-OFFICIAL",
-   author: '@_fearTeam',
+   packname: "BOT KINGDOM ❤️",
+   author: '@_BOTKINGDOMTeam',
     setting: {
     admin: "only admin",
     group: "only group",
